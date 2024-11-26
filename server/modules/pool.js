03ed073fe43ee7625abe890a3dbe50e88f04e8cd
Abdirizak Mohamed <inzaghi3650@gmail.com>
2024-11-26 03:09:51 -0600

    Match the db name of Jen to avoid conflict

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,7 +28,7 @@ else {
     pool = new pg.Pool({
         host: 'localhost',
         port: 5432,
-        database: 'Prime_Group',   // 	💥 Change this to the name of your database!
+        database: 'prime_group',   // 	💥 Change this to the name of your database!
     });
 }
 
