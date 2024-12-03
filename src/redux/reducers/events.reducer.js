966a1da1a1e2d8ecf6176b065c26b9978d7bc008
Padee Vang <vangpadee11@gmail.com>
2024-12-02 20:30:59 -0600

    edited something

--- src/redux/reducers/events.reducer.js
+++ src/redux/reducers/events.reducer.js
@@ -3,7 +3,9 @@ const eventsReducer = (
     action
   ) => {
     switch (action.type) {
+     
       case 'SET_EVENT':
+        console.log('what is in SET_EVENT', action.payload);
         return { ...state, events: [...state.events, action.payload] };
   
       case 'SET_LOADING':
